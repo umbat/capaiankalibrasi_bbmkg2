25283a6ece28b2fe9d2c97ef614e74ba78951148
tariyandy148 <150668613+tariyandy148@users.noreply.github.com>
2023-11-14 21:47:24 +0700

    Update hapus.php

--- page/aaws/hapus.php
+++ page/aaws/hapus.php
@@ -3,11 +3,11 @@ $id=$_GET['id'];
 
 //jika kondisi salam tanda kurung benar, maka jalankan perinta/ sekrip di dalam kuruang kurawal
 if($id!=''){
-$sql="delete from data_aws where id_aws='$id'";
+$sql="delete from data_aaws where id_aaws='$id'";
 $query=mysqli_query($con,$sql);
 	echo "<script type='text/javascript'>
 		alert('Data berhasil dihapus'); 
-		document.location='?page=aws.index';</script>";
+		document.location='?page=aaws.index';</script>";
 }else{
 	echo $nama_balai;
 	echo $nama_balai." masih kosong";	
